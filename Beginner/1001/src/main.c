c3bc838f21920dc3e8f9f4baff47962220ddee34
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2019-06-18 23:01:08 -0300

    [1001] - Implementação do core do problema 1001.

--- Beginner/1001/src/main.c
+++ Beginner/1001/src/main.c
@@ -5,7 +5,13 @@ int main(int argc, char *argv[])
     (void) argc;
     (void) argv;
 
-    puts("Aqui temos um texto.");
+    int a = 0;
+    int b = 0;
+
+    fscanf(stdin, "%d%*c", &a);
+    fscanf(stdin, "%d%*c", &b);
+
+    fprintf(stdout, "X = %d\n", a + b);
 
     return 0;
 }

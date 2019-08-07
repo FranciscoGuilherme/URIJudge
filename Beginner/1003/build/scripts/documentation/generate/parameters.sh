f553e19922da1215e705efd1da812edf9232a6fb
Francisco Guilherme Antunes <franciscoguilherme.mundo@outlook.com>
2019-08-07 07:24:48 -0300

    [1003] - Implementação dos scripts shell para gerar a documentação.
        - Correção do 'gap' de memória não liberada
        - Documentação do projeto 1002 através:
              - Código (doxygen)
              - Instruções de funcionamento (README)

---
+++ Beginner/1003/build/scripts/documentation/generate/parameters.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+readonly REPLACE_DISABLED=0
+readonly REPLACE_ACTIVATED=1
+readonly CONFIG=(
+    'OUTPUT_LANGUAGE'
+    'EXTRACT_ALL'
+    'RECURSIVE'
+    'SOURCE_BROWSER'
+    'GENERATE_TREEVIEW'
+)

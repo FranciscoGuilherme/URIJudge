4fbd61c0f0c87bb932510fc070656358c96ce79c
Francisco Guilherme Antunes <franciscoguilherme.mundo@outlook.com>
2019-08-07 07:52:22 -0300

    [1004] - Implementação dos scripts shell para gerar a documentação.
        - Correção do 'gap' de memória não liberada
        - Documentação do projeto 1004 através:
              - Código (doxygen)
              - Instruções de funcionamento (README)

--- Beginner/1004/src/headers/simple_mult.h
+++ Beginner/1004/src/headers/simple_mult.h
@@ -1,8 +1,59 @@
-#ifndef _simple_mult_h_
-#define _simple_mult_h_
+#ifndef _SIMPLE_MULT_H
+#define _SIMPLE_MULT_H
 
-void input(int *, int *);
-int mult(int *, int *);
-char *output(int);
+/**
+ * @~portuguese
+ * @brief Função responsável por ler os dados de entrada
+ *
+ * @param[out] a Primeiro número inteiro de entrada
+ * @param[out] b Segundo número inteiro de entrada
+ *
+ * @return void
+ *
+ * @~english
+ * @brief Function responsible for reading input data
+ *
+ * @param[out] a First Entry Integer
+ * @param[out] b Second Integer Input
+ *
+ * @return void
+ */
+void input(int *a, int *b);
+
+/**
+ * @~portuguese
+ * @brief Função responsável por efetuar a multiplicação dos dois valores inteiros a e b
+ *
+ * @param[out] a Primeiro número inteiro
+ * @param[out] b Segundo número inteiro
+ *
+ * @return Multiplicação de a e b
+ *
+ * @~english
+ * @brief Function responsible for multiplying the two integers a and b
+ *
+ * @param[out] a First Entry Integer
+ * @param[out] b Second Integer Input
+ *
+ * @return Multiplication of a and b
+ */
+int mult(int *a, int *b);
+
+/**
+ * @~portuguese
+ * @brief Função responsável por gerar a mensagem final de apresentação
+ *
+ * @param mult Resultado da multiplicação retornada por ::mult
+ *
+ * @return[out] Ponteiro para a mensagem final de apresentação
+ *
+ * @~english
+ * @brief Function responsible for generating the final presentation message
+ *
+ * @param mult Multiplication result returned by ::mult
+ *
+ * @return[out] Pointer to the final presentation message
+ */
+char *output(int mult);
 
 #endif

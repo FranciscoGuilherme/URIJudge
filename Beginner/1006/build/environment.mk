bdfc945c51a5d8de6a2b2484312edd42290451e3
Francisco Guilherme Antunes <franciscoguilherme.mundo@outlook.com>
2019-08-07 08:31:13 -0300

    [1006] - Implementação dos scripts shell para gerar a documentação.
        - Correção do 'gap' de memória não liberada
        - Documentação do projeto 1006 através:
              - Código (doxygen)
              - Instruções de funcionamento (README)

--- Beginner/1006/build/environment.mk
+++ Beginner/1006/build/environment.mk
@@ -2,8 +2,9 @@
 # PROJECT DIRECTORIES
 #---------------------------------------------------
 
-TESTS_DIR  := ../tests
-SOURCE_DIR := ../src
-BINARY_DIR := ../bin
-REPORT_DIR := ../report
-OBJECT_DIR := ../objects
+TESTS_DIR   := ../tests
+SOURCE_DIR  := ../src
+BINARY_DIR  := ../bin
+REPORT_DIR  := ../report
+OBJECT_DIR  := ../objects
+DOXYGEN_DIR := ../doxygen
--- Beginner/1006/src/main.c
+++ Beginner/1006/src/main.c
@@ -1,4 +1,21 @@
+/**
+ * @~portuguese
+ * @file main.c
+ * @author Francisco Guilherme A. S. Bessa
+ * @date 2019/08/07
+ * @brief
+ *      - Ponto de entrada da aplicação
+ *
+ * @~english
+ * @file main.c
+ * @author Francisco Guilherme A. S. Bessa
+ * @date 2019/08/07
+ * @brief
+ *      - Entrypoint
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "headers/average.h"
 
@@ -10,10 +27,15 @@ int main(int argc, char *argv[])
     double a = 0.0;
     double b = 0.0;
     double c = 0.0;
+    char *message = NULL;
 
     input(&a, &b, &c);
 
-    fprintf(stdout, "%s", output(average(&a, &b, &c)));
+    message = output(average(&a, &b));
+
+    fprintf(stdout, "%s", message);
+
+    free(message);
 
     return 0;
 }

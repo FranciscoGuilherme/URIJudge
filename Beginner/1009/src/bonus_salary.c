700d74099d4c777b92ea8474c5af2c43acfe7b7b
Francisco Guilherme Antunes <franciscoguilherme.mundo@outlook.com>
2019-08-09 11:07:08 -0300

    [1009] - Estrutura inicial para codificação do exercício 1009.

---
+++ Beginner/1009/src/bonus_salary.c
@@ -0,0 +1,8 @@
+/**
+ * @~portuguese
+ * @file salary.c
+ * @author Francisco Guilherme A. S. Bessa
+ * @date 2019/07/27
+ * @brief
+ *      - Contém a lógica de cálculo do salário de um funcionário
+ */
---
+++ Beginner/1009/src/main.c
@@ -0,0 +1,20 @@
+/**
+ * @~portuguese
+ * @file main.c
+ * @author Francisco Guilherme A. S. Bessa
+ * @date 2019/07/27
+ * @brief
+ *      - Ponto de entrada da aplicação
+ */
+
+#include <stdio.h>
+
+#include "headers/bonus_salary.h"
+
+int main(int argc, char *argv[])
+{
+    (void) argc;
+    (void) argv;
+
+    return 0;
+}

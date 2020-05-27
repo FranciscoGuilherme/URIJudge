2788b94b3ef0a4eaebde8f729ddf7843bda2509f
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2020-05-27 18:10:25 -0300

    [1020] - Finaliza implementação do exercício 1020 para submissão ao URI.

--- All/Beginner/1020.c
+++ All/Beginner/1020.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct time
+/*
+ * ------------------------------------
+ * ----- DEFINIÇÃO DAS ESTRUTURAS -----
+ * ------------------------------------
+ */
+
+typedef struct __attribute__((__packed__)) partial
+{
+    unsigned int months: 5;
+    unsigned int days: 5;
+} partial;
+
+typedef struct __attribute__((__packed__)) time
 {
     int years;
-    short int months;
-    short int days;
-};
+    struct partial partial;
+} time;
+
+/*
+ * ---------------------------------
+ * ----- PROTÓTIPO DAS FUNÇÕES -----
+ * ---------------------------------
+ */
 
 struct time *_gettime(int *);
 void output(struct time *);
+void destroy(struct time *);
+
+/*
+ * --------------------------------
+ * ----- ROTINA DE ENTRYPOINT -----
+ * --------------------------------
+ */
 
 int main(int argc, char *argv[])
 {
@@ -17,13 +41,23 @@ int main(int argc, char *argv[])
     (void) argv;
 
     int days = 0;
+    struct time *time = NULL;
 
     if (!fscanf(stdin, "%d%*c", &days)) exit(0);
 
-    output(_gettime(&days));
+    time = _gettime(&days);
+
+    output(time);
+    destroy(time);
 
     return 0;
 }
+
+/*
+ * --------------------------------------------
+ * ----- ROTINA DE PROCESSAMENTO DOS DIAS -----
+ * --------------------------------------------
+ */
 
 struct time *_gettime(int *days)
 {
@@ -31,25 +65,45 @@ struct time *_gettime(int *days)
 
     for (; *days >= 30; *days -= 30)
     {
-        time->months += 1;
+        time->partial.months += 1;
 
-        if (time->months == 12)
+        if (time->partial.months == 12)
         {
+            *days -= 5;
             time->years += 1;
-            time->months = 0;
+            time->partial.months = 0;
         }
     }
 
-    time->days = *days;
+    time->partial.days = *days;
 
     return time;
 }
 
+/*
+ * ----------------------------------
+ * ----- ROTINA DE APRESENTAÇÃO -----
+ * ----------------------------------
+ */
+
 void output(struct time *time)
 {
     fprintf(stdout, "%d ano(s)\n", time->years);
-    fprintf(stdout, "%d mes(es)\n", time->months);
-    fprintf(stdout, "%d dia(s)\n", time->days);
+    fprintf(stdout, "%d mes(es)\n", time->partial.months);
+    fprintf(stdout, "%d dia(s)\n", time->partial.days);
+
+    return;
+}
+
+/*
+ * ------------------------------------------
+ * ----- ROTINA DE LIBERAÇÃO DE MEMÓRIA -----
+ * ------------------------------------------
+ */
+
+void destroy(struct time *time)
+{
+    free(time);
 
     return;
 }
